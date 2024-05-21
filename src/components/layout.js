28b8cc6fae5f99f350206a0a3f36999074d963ed
oscar <oscar.vasquez@zentro.com.mx>
2024-05-21 16:00:46 -0600

    debug

---
+++ src/components/layout.js
@@ -0,0 +1,14 @@
+import Navbar from "./navbar";
+
+export default function Layout({ children }) {
+    const containerStyle = {
+        width: "90%",
+        margin: "100px auto"
+    };
+    return (
+        <div >
+            <Navbar />
+            <div style={containerStyle}>{children}</div>
+        </div>
+    )
+}
